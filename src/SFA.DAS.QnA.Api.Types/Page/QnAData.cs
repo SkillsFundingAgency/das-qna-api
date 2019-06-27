261b9283fa228b0bcd43d737e2cb48ed7bd927b2
Dave Gouge <davegouge@gmail.com>
2019-06-27 12:59:12 +0100

    Saving and validating answers

--- src/SFA.DAS.QnA.Api.Types/Page/QnAData.cs
+++ src/SFA.DAS.QnA.Api.Types/Page/QnAData.cs
@@ -4,8 +4,15 @@ namespace SFA.DAS.Qna.Api.Types.Page
 {
     public class QnAData
     {
+        public QnAData(){}
+
+        public QnAData(QnAData copyFrom)
+        {
+            this.RequestedFeedbackAnswered = copyFrom.RequestedFeedbackAnswered;
+            this.Pages = copyFrom.Pages;
+        }
+        
         public bool? RequestedFeedbackAnswered { get; set; }
         public List<Page> Pages { get; set; }
-        public FinancialApplicationGrade FinancialApplicationGrade { get; set; }
     }
 }
--- src/SFA.DAS.QnA.Api/Startup.cs
+++ src/SFA.DAS.QnA.Api/Startup.cs
@@ -18,7 +18,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using SFA.DAS.Qna.Data;
+using SFA.DAS.QnA.Api.Infrastructure;
 using SFA.DAS.QnA.Application;
+using SFA.DAS.QnA.Application.Validators;
 using SFA.DAS.QnA.Configuration.Config;
 using SFA.DAS.QnA.Configuration.Infrastructure;
 using Swashbuckle.AspNetCore.Swagger;
@@ -80,6 +82,9 @@ namespace SFA.DAS.QnA.Api
                     });
             }
 
+            services.RegisterAllTypes<IValidator>(new[] { typeof(IValidator).Assembly });
+            services.AddTransient<IValidatorFactory, ValidatorFactory>();
+            
             services.AddAutoMapper(typeof(HandlerResponse<>).Assembly);
             services.AddMediatR(AppDomain.CurrentDomain.GetAssemblies());
 
