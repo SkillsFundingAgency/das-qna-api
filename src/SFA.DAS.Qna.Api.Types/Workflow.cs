1870d47f2f1b63503717dd36a25b94e9fca776e8
David Gouge <davegouge@googlemail.com>
2019-10-07 12:12:23 +0100

    Removed status from Workflow.cs

--- src/SFA.DAS.Qna.Api.Types/Workflow.cs
+++ src/SFA.DAS.Qna.Api.Types/Workflow.cs
@@ -5,7 +5,6 @@ namespace SFA.DAS.QnA.Api.Types
     public class Workflow
     {
         public Guid Id { get; set; }
-        public string Status { get; set; }
         public string Description { get; set; }
         public string Version { get; set; }
         public string Type { get; set; }
