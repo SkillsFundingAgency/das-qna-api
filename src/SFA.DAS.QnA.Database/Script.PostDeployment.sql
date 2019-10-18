eeec95764418fcc3b2003d6638231af7c5c5f195
Martin Lush <martin-hornsby@ntlworld.com>
2019-10-18 07:59:52 +0100

    Added the appropriate files & folders ;)

---
+++ src/SFA.DAS.QnA.Database/Script.PostDeployment.sql
@@ -0,0 +1,11 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be appended to the build script.		
+ Use SQLCMD syntax to include a file in the post-deployment script.			
+ 
+ Example:      :r .\PostDeploymentScripts\myfile.sql												
+--------------------------------------------------------------------------------------
+*/
+
+-- END
