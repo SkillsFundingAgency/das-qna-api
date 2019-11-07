5b30b17b9750c47f47cdb92494fb885b79acd5d9
Alan Burns <adburns57@gmail.com>
2019-11-07 10:13:03 +0000

    Extend the size for Section Titles and Link Titles

--- src/SFA.DAS.QnA.Database/Tables/WorkflowSections.sql
+++ src/SFA.DAS.QnA.Database/Tables/WorkflowSections.sql
@@ -2,8 +2,8 @@ CREATE TABLE [dbo].[WorkflowSections](
 	[Id] [uniqueidentifier] NOT NULL,
 	[ProjectId] [uniqueidentifier] NOT NULL,
 	[QnAData] [nvarchar](max) NOT NULL,
-	[Title] [nvarchar](50) NOT NULL,
-	[LinkTitle] [nvarchar](50) NOT NULL,
+	[Title] [nvarchar](250) NOT NULL,
+	[LinkTitle] [nvarchar](250) NOT NULL,
 	[DisplayType] [nvarchar](50) NOT NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
