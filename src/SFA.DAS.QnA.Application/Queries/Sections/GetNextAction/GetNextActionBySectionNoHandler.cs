28ecdc686f56094046cee22d057b08edc2fbe8fc
Martin Lush <martin-hornsby@ntlworld.com>
2019-11-15 10:25:37 +0000

    Handle case when missing or no matching Next condition

--- src/SFA.DAS.QnA.Application/Queries/Sections/GetNextAction/GetNextActionBySectionNoHandler.cs
+++ src/SFA.DAS.QnA.Application/Queries/Sections/GetNextAction/GetNextActionBySectionNoHandler.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using SFA.DAS.QnA.Api.Types;
 using SFA.DAS.QnA.Application.Commands.SetPageAnswers;
 using SFA.DAS.QnA.Data;
+using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -33,9 +34,23 @@ namespace SFA.DAS.QnA.Application.Queries.Sections.GetNextAction
             if (page is null) return new HandlerResponse<GetNextActionResponse>(false, "Page does not exist");
 
             var answers = page.PageOfAnswers.SelectMany(a => a.Answers).ToList();
-            var nextAction = GetNextAction(page, answers, section, _dataContext);
 
-            return new HandlerResponse<GetNextActionResponse>(new GetNextActionResponse(nextAction.Action, nextAction.ReturnId));
+            try
+            {
+                var nextAction = GetNextAction(page, answers, section, _dataContext);
+                return new HandlerResponse<GetNextActionResponse>(new GetNextActionResponse(nextAction.Action, nextAction.ReturnId));
+            }
+            catch (ApplicationException)
+            {
+                if (page.Next is null || !page.Next.Any())
+                {
+                    return new HandlerResponse<GetNextActionResponse>(false, "There are no 'Next' instructions.");
+                }
+                else
+                {
+                    return new HandlerResponse<GetNextActionResponse>(false, "Cannot find a matching 'Next' instruction");
+                }
+            }
         }
     }
 }
