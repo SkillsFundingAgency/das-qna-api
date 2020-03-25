77b19ce79383630348ef358c8fff16c47ff3c0be
Michael Young <mick.young1981@googlemail.com>
2020-03-25 11:56:37 +0000

    Removed nullability from properties.

--- src/SFA.DAS.QnA.Application/Commands/Files/DownloadFile/DownloadFileBySectionNoRequest.cs
+++ src/SFA.DAS.QnA.Application/Commands/Files/DownloadFile/DownloadFileBySectionNoRequest.cs
@@ -7,8 +7,8 @@ namespace SFA.DAS.QnA.Application.Commands.Files.DownloadFile
     public class DownloadFileBySectionNoRequest : IRequest<HandlerResponse<DownloadFile>>
     {
         public Guid ApplicationId { get; }
-        public int? SequenceNo { get; }
-        public int? SectionNo { get; }
+        public int SequenceNo { get; }
+        public int SectionNo { get; }
         public string PageId { get; }
         public string QuestionId { get; }
         public string FileName { get; }
--- src/SFA.DAS.QnA.Application/Commands/Files/DownloadFile/DownloadFileRequest.cs
+++ src/SFA.DAS.QnA.Application/Commands/Files/DownloadFile/DownloadFileRequest.cs
@@ -7,7 +7,7 @@ namespace SFA.DAS.QnA.Application.Commands.Files.DownloadFile
     public class DownloadFileRequest : IRequest<HandlerResponse<DownloadFile>>
     {
         public Guid ApplicationId { get; }
-        public Guid? SectionId { get; }
+        public Guid SectionId { get; }
         public string PageId { get; }
         public string QuestionId { get; }
         public string FileName { get; }
