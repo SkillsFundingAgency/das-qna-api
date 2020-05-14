c15948bc4ce90434c01f612d96b6fbfe49891ae1
Michael Young <mick.young1981@googlemail.com>
2020-05-14 15:24:32 +0100

    Fixed broken test

--- src/SFA.DAS.QnA.Application.UnitTests/QueriesTests/GetSectionsTests/GetSectionsTestBase.cs
+++ src/SFA.DAS.QnA.Application.UnitTests/QueriesTests/GetSectionsTests/GetSectionsTestBase.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
+using SFA.DAS.QnA.Api.Types.Page;
 using SFA.DAS.QnA.Application.Queries.Sections.GetSections;
 using SFA.DAS.QnA.Application.Services;
 using SFA.DAS.QnA.Data;
@@ -27,7 +29,7 @@ namespace SFA.DAS.QnA.Application.UnitTests.QueriesTests.GetSectionsTests
 
             ApplicationId = Guid.NewGuid();
 
-            context.Applications.Add(new Data.Entities.Application { Id = ApplicationId });
+            context.Applications.Add(new Data.Entities.Application { Id = ApplicationId, ApplicationData = "{}"});
 
             context.ApplicationSequences.AddRange(new[]
             {
@@ -37,9 +39,9 @@ namespace SFA.DAS.QnA.Application.UnitTests.QueriesTests.GetSectionsTests
 
             context.ApplicationSections.AddRange(new[]
             {
-                new ApplicationSection {ApplicationId = ApplicationId, SequenceNo = 1, SectionNo = 1},
-                new ApplicationSection {ApplicationId = ApplicationId, SequenceNo = 1, SectionNo = 2},
-                new ApplicationSection {ApplicationId = ApplicationId, SequenceNo = 2, SectionNo = 1}
+                new ApplicationSection {ApplicationId = ApplicationId, SequenceNo = 1, SectionNo = 1, QnAData = new QnAData { Pages = new List<Page>()} },
+                new ApplicationSection {ApplicationId = ApplicationId, SequenceNo = 1, SectionNo = 2, QnAData = new QnAData { Pages = new List<Page>()} },
+                new ApplicationSection {ApplicationId = ApplicationId, SequenceNo = 2, SectionNo = 1, QnAData = new QnAData { Pages = new List<Page>()} }
             });
 
             await context.SaveChangesAsync();
