939083bdf4a23b8cb5d5d28b9b538ae84bf9b228
Martin Lush <martin-hornsby@ntlworld.com>
2020-09-23 09:40:56 +0100

    APR-1880 - Give PRIMARY KEY constraints a useful name

--- src/SFA.DAS.QnA.Database/Tables/ApplicationSections.sql
+++ src/SFA.DAS.QnA.Database/Tables/ApplicationSections.sql
@@ -1,6 +1,6 @@
 CREATE TABLE [ApplicationSections]
 (
-	[Id] [uniqueidentifier] NOT NULL PRIMARY KEY DEFAULT NEWID(),
+	[Id] [uniqueidentifier] NOT NULL DEFAULT NEWID(),
 	[ApplicationId] [uniqueidentifier] NOT NULL,
 	[SequenceNo] [int] NOT NULL,
 	[SectionNo] [int] NOT NULL,
@@ -8,7 +8,8 @@ CREATE TABLE [ApplicationSections]
 	[Title] [nvarchar](250) NOT NULL,
 	[LinkTitle] [nvarchar](250) NOT NULL,
 	[DisplayType] [nvarchar](50) NOT NULL,
-    [SequenceId] [uniqueidentifier] NOT NULL
+    [SequenceId] [uniqueidentifier] NOT NULL,
+    CONSTRAINT [PK_ApplicationSections] PRIMARY KEY ( [Id] )
 ) 
 GO
 
