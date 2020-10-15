0dc9fb477800191819200988b1e837ebeb01e2bb
chrisfoster186 <chrisfoster186@googlemail.com>
2020-10-15 09:08:39 +0100

    wip

--- src/SFA.DAS.QnA.Application/Queries/Sequences/GetSequences/GetSequencesHandler.cs
+++ src/SFA.DAS.QnA.Application/Queries/Sequences/GetSequences/GetSequencesHandler.cs
@@ -24,37 +24,40 @@ namespace SFA.DAS.QnA.Application.Queries.Sequences.GetSequences
         
         public async Task<HandlerResponse<List<Sequence>>> Handle(GetSequencesRequest request, CancellationToken cancellationToken)
         {
-            var application = _dataContext.Applications.Single(x => x.Id == request.ApplicationId);
+            var sequences = await _dataContext.ApplicationSequences.AsNoTracking()
+                .Where(seq => seq.ApplicationId == request.ApplicationId)
+                .ToListAsync(cancellationToken: cancellationToken);
 
-            var workflowSequences = await _dataContext.WorkflowSequences.AsNoTracking()
-                .Where(x=> x.WorkflowId == application.WorkflowId)
-                .ToListAsync(cancellationToken);
-
-            var groupedSequences = workflowSequences.GroupBy(seq => new { seq.SequenceNo, seq.IsActive }).ToList();
-
-            var newApplicationSequences = groupedSequences.Select(seq => new ApplicationSequence
+            if (!sequences.Any())
             {
-                ApplicationId = request.ApplicationId,
-                SequenceNo = seq.Key.SequenceNo,
-                IsActive = seq.Key.IsActive
-            }).ToList();
+                return new HandlerResponse<List<Sequence>>(false, "Application does not exist");
+            }
 
+            var mappedSequences = _mapper.Map<List<Sequence>>(sequences);
 
-            var mappedSequences = _mapper.Map<List<Sequence>>(newApplicationSequences);
             return new HandlerResponse<List<Sequence>>(mappedSequences);
-
-            //var sequences = await _dataContext.ApplicationSequences.AsNoTracking()
-            //    .Where(seq => seq.ApplicationId == request.ApplicationId)
-            //    .ToListAsync(cancellationToken: cancellationToken);
-
-            //if (!sequences.Any())
-            //{
-            //    return new HandlerResponse<List<Sequence>>(false, "Application does not exist");
-            //}
-
-            //var mappedSequences = _mapper.Map<List<Sequence>>(sequences);
-
-            //return new HandlerResponse<List<Sequence>>(mappedSequences);
         }
+
+        //public async Task<HandlerResponse<List<Sequence>>> Handle(GetSequencesRequest request, CancellationToken cancellationToken)
+        //{
+        //    var application = _dataContext.Applications.Single(x => x.Id == request.ApplicationId);
+
+        //    var workflowSequences = await _dataContext.WorkflowSequences.AsNoTracking()
+        //        .Where(x => x.WorkflowId == application.WorkflowId)
+        //        .ToListAsync(cancellationToken);
+
+        //    var groupedSequences = workflowSequences.GroupBy(seq => new { seq.SequenceNo, seq.IsActive }).ToList();
+
+        //    var newApplicationSequences = groupedSequences.Select(seq => new ApplicationSequence
+        //    {
+        //        ApplicationId = request.ApplicationId,
+        //        SequenceNo = seq.Key.SequenceNo,
+        //        IsActive = seq.Key.IsActive
+        //    }).ToList();
+
+
+        //    var mappedSequences = _mapper.Map<List<Sequence>>(newApplicationSequences);
+        //    return new HandlerResponse<List<Sequence>>(mappedSequences);
+        //}
     }
 }
