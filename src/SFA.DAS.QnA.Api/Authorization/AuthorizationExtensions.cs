24ae3ce541fa7213b1b17d937ab406e88322b7f3
Martin Lush <martin-hornsby@ntlworld.com>
2021-03-08 11:52:36 +0000

    Allow multiple Active Directory managed identities

---
+++ src/SFA.DAS.QnA.Api/Authorization/AuthorizationExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+
+namespace SFA.DAS.QnA.Api.Authorization
+{
+    public static class AuthorizationExtensions
+    {
+        public static IServiceCollection AddApiAuthorization(this IServiceCollection services, IHostingEnvironment environment)
+        {
+            var isDevelopment = environment.IsDevelopment();
+
+            services.AddAuthorization(x =>
+            {
+                x.AddPolicy("default", policy =>
+                {
+                    if (isDevelopment)
+                    {
+                        policy.AllowAnonymousUser();
+                    }
+                    else
+                    {
+                        policy.RequireAuthenticatedUser();
+                    }
+                });
+
+                x.DefaultPolicy = x.GetPolicy("default");
+            });
+
+            if (isDevelopment)
+            {
+                services.AddSingleton<IAuthorizationHandler, LocalAuthorizationHandler>();
+            }
+
+            return services;
+        }
+    }
+}
