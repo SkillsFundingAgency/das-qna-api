d7345c29eb9f268d739745059d58993d975d29ee
Martin Lush <martin-hornsby@ntlworld.com>
2021-03-08 13:54:47 +0000

    Revert "Changing variable name to ValidAudiences as its more meaningful"
    This reverts commit 8e4b82112cc4a1b045d887fa8103f1549b394179.

--- src/SFA.DAS.QnA.Configuration/Config/AzureActiveDirectoryConfiguration.cs
+++ src/SFA.DAS.QnA.Configuration/Config/AzureActiveDirectoryConfiguration.cs
@@ -3,6 +3,6 @@
     public class AzureActiveDirectoryConfiguration
     {
         public string Tenant { get; set; }
-        public string ValidAudiences { get; set; }
+        public string Identifier { get; set; }
     }
 }
