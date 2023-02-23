14fa20a27b74fd4eb4fd5f3af71560bb5da84769
Sarah Duncan <sarah.coetzee@education.gov.uk>
2023-02-23 10:38:13 +0000

    QF-1001 add unit tests for ApplicationDataController

---
+++ src/SFA.DAS.QnA.Api.UnitTests/ApplicationDataControllerTests/WhenGettingApplicationData.cs
@@ -0,0 +1,93 @@
+﻿using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using NSubstitute;
+using NUnit.Framework;
+using SFA.DAS.QnA.Api.Types;
+using SFA.DAS.QnA.Application.Queries.ApplicationData.GetApplicationData;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.QnA.Api.UnitTests.ApplicationDataControllerTests
+{
+    [TestFixture]
+    public class WhenGettingApplicationData
+    {
+        [Test]
+        public async Task And_MediatorCallFails_ThenNotFoundResultReturned()
+        {
+            var mediator = Substitute.For<IMediator>();
+            var controller = new Controllers.ApplicationDataController(mediator);
+            mediator.Send(Arg.Any<GetApplicationDataRequest>()).Returns(new HandlerResponse<string>() { Success = false, Message = "Application does not exist." });
+
+            var result = await controller.Get(Guid.NewGuid());
+
+            result.Result.Should().BeOfType<NotFoundObjectResult>();
+        }
+
+        [Test]
+        public async Task And_MediatorCallIsSuccessful_ThenOkObjectResultReturned()
+        {
+            var mediator = Substitute.For<IMediator>();
+            var controller = new Controllers.ApplicationDataController(mediator);
+            var applicationData = File.ReadAllText("ApplicationDataControllerTests/test.json");
+            var applicationDataDeserialized = JsonConvert.DeserializeObject(applicationData);
+            mediator.Send(Arg.Any<GetApplicationDataRequest>()).Returns(new HandlerResponse<string>(applicationData));
+
+            var result = await controller.Get(Guid.NewGuid());
+            ObjectResult objectResult = (ObjectResult)result.Result;
+
+            result.Result.Should().BeOfType<OkObjectResult>();
+            objectResult.Value.Should().BeEquivalentTo(applicationDataDeserialized);
+        }
+
+        [Test]
+        public async Task And_MediatorCallIsSuccessful_Then_Result_Contains_ExpectedProperties()
+        {
+            var applicationData = File.ReadAllText("ApplicationDataControllerTests/test.json");
+            var mediator = Substitute.For<IMediator>();
+            mediator.Send(Arg.Any<GetApplicationDataRequest>()).Returns(new HandlerResponse<string>(applicationData));
+            var controller = new Controllers.ApplicationDataController(mediator);
+
+            var actionResult = await controller.Get(Guid.NewGuid());
+            var okResult = (OkObjectResult)actionResult.Result;
+            var resultAsJson = JsonDocument.Parse(okResult.Value.ToString()).RootElement;
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(resultAsJson.GetProperty("OrganisationReferenceId")
+                                        .GetString(),
+                                        Is.EqualTo("c3333b62-a07c-415e-8778-84222231b0s1"));
+
+                Assert.That(resultAsJson.GetProperty("TradingName")
+                                        .ValueKind,
+                                        Is.EqualTo(JsonValueKind.Null));
+
+                Assert.That(resultAsJson.GetProperty("UseTradingName")
+                                        .GetBoolean(),
+                                        Is.False);
+
+                Assert.That(resultAsJson.GetProperty("company_number")
+                                        .GetString(),
+                                        Is.Empty);
+
+                Assert.That(resultAsJson.GetProperty("CompanySummary")
+                                        .GetProperty("CompanyNumber")
+                                        .GetString(),
+                                        Is.EqualTo("RC123456"));
+
+                Assert.That(resultAsJson.GetProperty("CharitySummary")
+                                        .GetProperty("Trustees")
+                                        .EnumerateArray()
+                                        .First()
+                                        .GetProperty("Name")
+                                        .GetString(),
+                                        Is.EqualTo("DR DOOM"));
+            });
+        }
+    }
+}
---
+++ src/SFA.DAS.QnA.Api.UnitTests/ApplicationDataControllerTests/WhenPostingApplicationData.cs
@@ -0,0 +1,79 @@
+﻿using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using NSubstitute;
+using NUnit.Framework;
+using SFA.DAS.QnA.Api.Controllers;
+using SFA.DAS.QnA.Api.Types;
+using SFA.DAS.QnA.Application.Commands.SetApplicationData;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.QnA.Api.UnitTests.ApplicationDataControllerTests
+{
+    public class WhenPostingApplicationData
+    {
+        [Test]
+        public async Task And_MediatorCallFails_ThenNotFoundResultReturned()
+        {
+            var mediator = Substitute.For<IMediator>();
+            var controller = new ApplicationDataController(mediator);
+            var applicationData = File.ReadAllText("ApplicationDataControllerTests/test.json");
+            mediator.Send(Arg.Any<SetApplicationDataRequest>()).Returns(new HandlerResponse<string>() { Success = false, Message = "ApplicationData does not validated against the Project's Schema." });
+
+            var result = await controller.Post(Guid.NewGuid(), applicationData);
+
+            result.Result.Should().BeOfType<NotFoundObjectResult>();
+        }
+
+        [Test]
+        public async Task And_MediatorCallIsSuccessful_ThenDeserializedResultReturned()
+        {
+            var mediator = Substitute.For<IMediator>();
+            var controller = new ApplicationDataController(mediator);
+            var applicationData = File.ReadAllText("ApplicationDataControllerTests/test.json");
+            var applicationDataDeserialized = JsonConvert.DeserializeObject(applicationData);
+            mediator.Send(Arg.Any<SetApplicationDataRequest>()).Returns(new HandlerResponse<string>(applicationData));
+
+            var actionResult = await controller.Post(Guid.NewGuid(), applicationData);
+            var resultAsJson = JsonDocument.Parse(actionResult.Value.ToString()).RootElement;
+
+            actionResult.Value.Should().BeEquivalentTo(applicationDataDeserialized);
+            Assert.Multiple(() =>
+            {
+                Assert.That(resultAsJson.GetProperty("OrganisationReferenceId")
+                                        .GetString(),
+                                        Is.EqualTo("c3333b62-a07c-415e-8778-84222231b0s1"));
+
+                Assert.That(resultAsJson.GetProperty("TradingName")
+                                        .ValueKind,
+                                        Is.EqualTo(JsonValueKind.Null));
+
+                Assert.That(resultAsJson.GetProperty("UseTradingName")
+                                        .GetBoolean(),
+                                        Is.False);
+
+                Assert.That(resultAsJson.GetProperty("company_number")
+                                        .GetString(),
+                                        Is.Empty);
+
+                Assert.That(resultAsJson.GetProperty("CompanySummary")
+                                        .GetProperty("CompanyNumber")
+                                        .GetString(),
+                                        Is.EqualTo("RC123456"));
+
+                Assert.That(resultAsJson.GetProperty("CharitySummary")
+                                        .GetProperty("Trustees")
+                                        .EnumerateArray()
+                                        .First()
+                                        .GetProperty("Name")
+                                        .GetString(),
+                                        Is.EqualTo("DR DOOM"));
+            });
+        }
+    }
+}
