f2d3e6903aeff2722033255667776f3e153369f3
Sarah Duncan <sarah.coetzee@education.gov.uk>
2023-03-02 10:24:45 +0000

    QF-1001 PR changes

--- src/SFA.DAS.QnA.Api.Types/Page/Page.cs
+++ src/SFA.DAS.QnA.Api.Types/Page/Page.cs
@@ -1,7 +1,7 @@
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using JsonIgnoreAttribute = System.Text.Json.Serialization.JsonIgnoreAttribute;
 
 namespace SFA.DAS.QnA.Api.Types.Page
 {
